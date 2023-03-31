49fed2edaf3c798817cfc74d8f743759eee15bfa
Shishir <shishirchaurasiya435@gmail.com>
2023-04-01 03:34:42 +0530

    title change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       }}
     >
       <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-        <img src={logo} alt="logo" height={45} /> <span style={{color: "white", fontSize: "45px", marginLeft: '5px', marginRight: '5px'}}>Clone</span>
+        <img src={logo} alt="logo" height={45} /> <span style={{ color: "white", fontSize: "45px", marginLeft: '5px', marginRight: '5px' }}>&nbsp; &nbsp;YT</span>
       </Link>
       <SearchBar />
     </Stack>
